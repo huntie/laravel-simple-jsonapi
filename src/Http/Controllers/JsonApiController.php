8d2b83a3cb013f78d81d5f73fb4574500e5ed07f
Alex Hunt <hello@alexhunt.io>
2016-06-05 13:56:50 +0100

    Include AuthorizesRequests and ValidatesRequests in controller

--- src/Http/Controllers/JsonApiController.php
+++ src/Http/Controllers/JsonApiController.php
@@ -9,13 +9,15 @@ use Illuminate\Database\QueryException;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 
 abstract class JsonApiController extends Controller
 {
-    use JsonApiErrors, JsonApiTransforms;
+    use JsonApiErrors, JsonApiTransforms, AuthorizesRequests, ValidatesRequests;
 
     /**
      * Return the Eloquent Model for the resource.
