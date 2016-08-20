556ef8cdf8a472a22b46b8afbb66e7ed53d4a037
Alex Hunt <hello@alexhunt.io>
2016-08-20 12:00:03 +0100

    Add remaining object and relation transformation logic to JsonApiSerializer

--- src/Serializers/JsonApiSerializer.php
+++ src/Serializers/JsonApiSerializer.php
@@ -2,6 +2,9 @@
 
 namespace Huntie\JsonApi\Serializers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
 class JsonApiSerializer
 {
     /**
@@ -12,6 +15,13 @@ class JsonApiSerializer
     protected $record;
 
     /**
+     * The relationships to return.
+     *
+     * @var array
+     */
+    protected $relationships;
+
+    /**
      * The subset of record attributes to return.
      *
      * @var array
@@ -19,7 +29,7 @@ class JsonApiSerializer
     protected $fields;
 
     /**
-     * The record relations to include.
+     * The relationships to load and include.
      *
      * @var array
      */
@@ -40,13 +50,6 @@ class JsonApiSerializer
     protected $links;
 
     /**
-     * The loaded records to include.
-     *
-     * @var \Illuminate\Support\Collection
-     */
-    protected $included;
-
-    /**
      * Create a new JSON API resource serializer.
      *
      * @param Model      $record  The model instance to serialise
@@ -56,11 +59,11 @@ class JsonApiSerializer
     public function __construct($record, array $fields = [], array $include = [])
     {
         $this->record = $record;
+        $this->relationships = array_merge($record->getRelations(), $include);
         $this->fields = array_unique($fields);
-        $this->include = array_unique(array_merge($record->getRelations(), $include));
+        $this->include = array_unique($include);
         $this->meta = collect([]);
         $this->links = collect([]);
-        $this->included = collect([]);
     }
 
     /**
@@ -70,7 +73,7 @@ class JsonApiSerializer
      */
     public function scopeIncludes($include)
     {
-        $this->include->intersect($include);
+        $this->include = array_intersect($this->include, $include);
     }
 
     /**
@@ -81,7 +84,7 @@ class JsonApiSerializer
      */
     public function addMeta($key, $value = null)
     {
-        $this->meta->merge(is_array($key) ? $key : [$key => $value]);
+        $this->meta = $this->meta->merge(is_array($key) ? $key : [$key => $value]);
     }
 
     /**
@@ -92,7 +95,7 @@ class JsonApiSerializer
      */
     public function addLinks($key, $value = null)
     {
-        $this->links->merge(is_array($key) ? $key : [$key => $value]);
+        $this->links = $this->links->merge(is_array($key) ? $key : [$key => $value]);
     }
 
     /**
@@ -115,9 +118,12 @@ class JsonApiSerializer
      */
     public function toResourceObject()
     {
-        return array_merge($this->toResourceIdentifier(), [
+        $this->record->load($this->relationships);
+
+        return array_merge($this->toResourceIdentifier(), array_filter([
             'attributes' => $this->transformRecordAttributes(),
-        ]);
+            'relationships' => $this->transformRecordRelations(),
+        ]));
     }
 
     /**
@@ -129,9 +135,9 @@ class JsonApiSerializer
     {
         return array_filter([
             'data' => $this->toResourceObject(),
-            'included' => $this->included->isEmpty() ? null : $this->included->toArray(),
-            'links' => $this->links->isEmpty() ? null : $this->links->toArray(),
-            'meta' => $this->meta->isEmpty() ? null : $this->meta->toArray(),
+            'included' => $this->transformIncludedRelations(),
+            'links' => $this->links->toArray(),
+            'meta' => $this->meta->toArray(),
         ]);
     }
 
@@ -170,5 +176,52 @@ class JsonApiSerializer
         }
 
         return $attributes;
+    }
+
+    /**
+     * Return a collection of JSON API resource identifier objects by each
+     * relation on the primary record.
+     *
+     * @return array
+     */
+    protected function transformRecordRelations()
+    {
+        $relationships = [];
+
+        foreach ($this->relationships as $relation) {
+            $relation = $this->record->{$relation};
+            $data = [];
+
+            if ($relation instanceof Collection) {
+                $data = array_map(function ($record) {
+                    return (new static($record))->toResourceIdentifier();
+                }, $relation);
+            } else if ($relation instanceof Model) {
+                $data = (new static($relation))->toResourceIdentifier();
+            }
+
+            $relationships[$relation] = compact('data');
+        }
+
+        return $relationships;
+    }
+
+    /**
+     * Return a collection of JSON API resource objects for each included
+     * relationship.
+     *
+     * @return array
+     */
+    protected function transformIncludedRelations()
+    {
+        $included = collect([]);
+
+        foreach ($this->include as $relation) {
+            $included = $included->merge(array_map(function ($record) {
+                return $record ? (new static($record))->toResourceObject() : null;
+            }, (array) $this->record->{$relation}));
+        }
+
+        return array_filter($included->toArray());
     }
 }
