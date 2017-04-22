221624e84387194c8e7ce874029b38d1d0a2d101
Alex Hunt <hello@alexhunt.io>
2017-04-22 14:29:58 +0100

    Extend ResourceRegistrar with JSON API relationship actions

--- src/Routing/ResourceRegistrar.php
+++ src/Routing/ResourceRegistrar.php
@@ -12,6 +12,13 @@ class ResourceRegistrar extends \Illuminate\Routing\ResourceRegistrar
     protected $resourceDefaults = ['index', 'store', 'show', 'update', 'destroy'];
 
     /**
+     * The default actions for each named resource relationship.
+     *
+     * @var array
+     */
+    protected $relationshipDefaults = ['show'];
+
+    /**
      * Create a new resource registrar instance.
      *
      * @param \Huntie\JsonApi\Routing\Router $router
@@ -19,5 +26,102 @@ class ResourceRegistrar extends \Illuminate\Routing\ResourceRegistrar
     public function __construct(Router $router)
     {
         $this->router = $router;
+    }
+
+    /**
+     * Route a resource to a controller.
+     *
+     * @param string $name
+     * @param string $controller
+     * @param array  $options
+     */
+    public function register($name, $controller, array $options = [])
+    {
+        parent::register($name, $controller, $options);
+
+        $this->registerRelationships($name, $controller, $options);
+    }
+
+    /**
+     * Route any resource relationships to a controller.
+     *
+     * @param string $name
+     * @param string $controller
+     * @param array  $options
+     */
+    protected function registerRelationships($name, $controller, array $options)
+    {
+        $base = $this->getResourceWildcard(last(explode('.', $name)));
+
+        // Map non-associative members as keys, with default relationship actions
+        $relationships = collect(array_get($options, 'relationships', []))
+            ->mapWithKeys(function ($methods, $relationship) {
+                return is_numeric($relationship)
+                    ? [$methods => $this->relationshipDefaults]
+                    : [$relationship => (array) $methods];
+            });
+
+        foreach (['show', 'update'] as $action) {
+            $matched = $relationships->filter(function ($methods) use ($action) {
+                return in_array($action, $methods);
+            })->keys()->toArray();
+
+            if (!empty($matched)) {
+                $this->{'addRelationship' . ucfirst($action)}($name, $base, $matched, $controller, $options);
+            }
+        }
+    }
+
+    /**
+     * Add a relationship show method to match named relationships on the resource.
+     *
+     * @param string $name
+     * @param string $base
+     * @param array  $relationships
+     * @param string $controller
+     * @param array  $options
+     *
+     * @return \Illuminate\Routing\Route
+     */
+    protected function addRelationshipShow($name, $base, array $relationships, $controller, array $options)
+    {
+        $uri = $this->getRelationshipUri($name, $base);
+        $action = $this->getResourceAction($name, $controller, 'showRelationship', $options);
+
+        return $this->router->get($uri, $action)
+            ->where('relationship', '(' . implode(')|(', $relationships) . ')');
+    }
+
+    /**
+     * Add a relationship update method to match named relationships on the resource.
+     *
+     * @param string $name
+     * @param string $base
+     * @param array  $relationships
+     * @param string $controller
+     * @param array  $options
+     *
+     * @return \Illuminate\Routing\Route
+     */
+    protected function addRelationshipUpdate($name, $base, array $relationships, $controller, array $options)
+    {
+        $uri = $this->getRelationshipUri($name, $base);
+        $action = $this->getResourceAction($name, $controller, 'updateRelationship', $options);
+
+        return $this->router->match(['PUT', 'PATCH'], $uri, $action)
+            ->where('relationship', '(' . implode(')|(', $relationships) . ')');
+    }
+
+    /**
+     * Get the URI for resource relationships.
+     *
+     * @param string $name
+     * @param string $base
+     *
+     * @return string
+     */
+    public function getRelationshipUri($name, $base)
+    {
+        return sprintf('%s/{%s}/relationships/{relationship}', $this->getResourceUri($name), $base);
     }
 }
--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -4,4 +4,21 @@ namespace Huntie\JsonApi\Routing;
 
 class Router extends \Illuminate\Routing\Router
 {
+    /**
+     * Route a resource to a controller.
+     *
+     * @param string $name
+     * @param string $controller
+     * @param array  $options
+     */
+    public function resource($name, $controller, array $options = [])
+    {
+        if ($this->container && $this->container->bound(ResourceRegistrar::class)) {
+            $registrar = $this->container->make(ResourceRegistrar::class);
+        } else {
+            $registrar = new ResourceRegistrar($this);
+        }
+
+        $registrar->register($name, $controller, $options);
+    }
 }
