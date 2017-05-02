f08152a835817c2632fd54df5fb61c7e30c75f43
Alex Hunt <hello@alexhunt.io>
2017-05-02 17:24:38 +0100

    Update provider reference in base TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -28,7 +28,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageProviders($app)
     {
         return [
-            \Huntie\JsonApi\JsonApiServiceProvider::class,
+            \Huntie\JsonApi\Providers\JsonApiServiceProvider::class,
         ];
     }
 }
