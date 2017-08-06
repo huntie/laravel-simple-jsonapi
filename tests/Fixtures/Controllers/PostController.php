5d7d8c6150b03bb31e27f56c376a670550ef940b
Alex Hunt <hello@alexhunt.io>
2017-08-06 13:44:05 +0100

    Configure base TestCase with app routes and test database

---
+++ tests/Fixtures/Controllers/PostController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Fixtures\Controllers;
+
+use Huntie\JsonApi\Http\Controllers\JsonApiController;
+use Huntie\JsonApi\Http\Controllers\JsonApiControllerActions;
+
+class PostController extends JsonApiController
+{
+    use JsonApiControllerActions;
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,13 +9,38 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     use JsonApiAssertions;
 
     /**
-     * Setup the test environment.
+     * Set up the test environment.
      */
     public function setUp()
     {
         parent::setUp();
 
         $this->withFactories(__DIR__ . '/Support/Factories');
+
+        $this->loadMigrationsFrom([
+            '--database' => 'testbench',
+            '--realpath' => realpath(__DIR__ . '/database/migrations'),
+        ]);
+    }
+
+    /**
+     * Define Testbench app environment.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
+    public function getEnvironmentSetup($app)
+    {
+        $app['config']->set([
+            'database.default' => 'testbench',
+            'database.connections.testbench' => [
+                'driver'   => 'sqlite',
+                'database' => ':memory:',
+                'prefix'   => '',
+            ],
+            'jsonapi.model_namespace' => 'Tests\Fixtures\Models',
+        ]);
+
+        $this->registerAppRoutes($app);
     }
 
     /**
@@ -28,7 +53,21 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageProviders($app)
     {
         return [
+            \Orchestra\Database\ConsoleServiceProvider::class,
             \Huntie\JsonApi\Providers\JsonApiServiceProvider::class,
         ];
+    }
+
+    /**
+     * Register test application routes.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function registerAppRoutes($app)
+    {
+        $app['router']->resources([
+            'users' => \Tests\Fixtures\Controllers\UserController::class,
+            'posts' => \Tests\Fixtures\Controllers\PostController::class,
+        ]);
     }
 }
